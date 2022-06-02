5b5d4d3c757052615f8acd1e5fad0b866384216e
varsharamanujam <60292603+varsharamanujam@users.noreply.github.com>
2022-06-02 15:23:57 +0530

    Delete src/main/java/springboot/web/raw directory

--- src/main/java/springboot/web/raw/models/Root.java
+++
@@ -1,8 +0,0 @@
-package springboot.web.raw.models;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Root{
-    public List<Program> programs = new ArrayList<>();
-}
--- src/main/java/springboot/web/raw/models/Soloist.java
+++
@@ -1,7 +0,0 @@
-package springboot.web.raw.models;
-
-public class Soloist{
-    public String soloistName;
-    public String soloistRoles;
-    public String soloistInstrument;
-}
